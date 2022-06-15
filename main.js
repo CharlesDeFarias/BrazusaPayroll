e7ee8a2e95906724c65277796cee83c13e097afe
CharlesDeFarias <47393024+CharlesDeFarias@users.noreply.github.com>
2022-06-15 17:39:34 -0400

    Add files via upload
    Very first step towards payroll app

---
+++ main.js
@@ -0,0 +1,6 @@
+//Main goal: Payroll app that allows input and modification of hours, generates and saves weekly reports, generates whatsapp message
+//First goal: Input payroll for each week and save into history page, Second goal: Report generation page, creates filterable report, Integrate with google sheet and add time reported tracking, Automate picture & whatsapp message payroll reporter, Convert to React and add support for automatically adding/removing employees, Add mobile support. Style and dragging/clicking/etc
+//Current goal: First goal: Input payroll for each week and save into history page,
+//Click into cells to add or change hours
+//Make input bar work, be able to add a single employee's day and hour
+//
